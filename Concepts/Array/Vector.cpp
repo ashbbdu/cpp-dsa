e186153d23f9486b92340f10a8d448be768c3168
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-07-21 23:49:41 +0530

    arrays

--- Concepts/Array/Vector.cpp
+++ Concepts/Array/Vector.cpp
@@ -6,9 +6,34 @@ int main () {
 
     // declaring a vector
     vector <int> arr = {1,2,3,4};
+    arr.push_back(5);
     vector <int> crr (10,-1);
-    for (size_t i = 0; i < crr.size() ; i++)  {
+    cout << "arr ka data" << endl;
+    cout << "Size of arr is " << arr.size() << endl;
+    cout << "Capacity of arr is " << arr.capacity() << endl;
+    for (int i = 0; i < crr.size() ; i++)  {
         cout << crr[i] << " ";
     }
-    
+    cout << endl;
+    cout << "Enter the size of array" << endl;
+    int n;
+    cin >> n;
+    vector <int> brr(n);
+    cout << brr.size() << endl;
+    brr.push_back(5);
+    brr.push_back(10);
+    // brr.pop_back();
+    cout << arr.empty() << " is empty" << endl;
+    cout << brr.size() << endl;
+    cout << brr[3] << endl;
+
+     for (int i = 0; i < brr.size() ; i++)  {
+        cout << brr[i] << " ";
+    }
+    cout << endl;
+    cout << brr.capacity() << endl; //should be double of size()
+
+
+    vector <int> drr;
+    cout << drr.empty() << endl;
 }

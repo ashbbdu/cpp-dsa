d1dd2a5765d7be91ca0aecc778ce9899a8dea25f
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-07-24 00:53:49 +0530

    2d arrays

--- Concepts/Array/2DArray/Problems/MaxandMinElem.cpp
+++ Concepts/Array/2DArray/Problems/MaxandMinElem.cpp
@@ -13,14 +13,27 @@ int findMinimum (int arr[][3]) {
     return mini;
 }
 
+int findMaximum (int arr[][3]) {
+    int maxi = INT_MIN;
+    for(int i = 0 ; i < 3 ; i++) {
+        for(int j = 0 ; j < 3 ; j++) {
+            if(arr[i][j] > maxi) {
+                maxi = arr[i][j];
+            }
+        }
+    }
+    return maxi;
+}
+
 int main () {
         int arr[3][3] = {
         {1,2,3},
         {4,5,6},
-        {3,4,-1}
+        {3,7,-1}
     };
     // Find Minimum Element
     cout << findMinimum(arr) << " is the minimum element" << endl;
-    
+    // Find Maximum Element
+    cout << findMaximum(arr) << " is the maximum element" <<  endl;    
     
 }
